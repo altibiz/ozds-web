2aec74671ee3244af1e8424ccb83059e0df4ac49
Hrvoje Juric <hrvoje@altibiz.com>
2022-03-17 21:42:21 +0100

    remove accidentally added UgpWeb

--- UgpWeb/Sqls/emptyUGpLocal.sql
+++
@@ -1,6 +0,0 @@
-DELETE
-  FROM [UgpWeb].[glaspoduzetnikah_idea].[all_members]
-DELETE
-  FROM [UgpWeb].[glaspoduzetnikah_idea].bank_statements
-  DELETE
-  FROM [UgpWeb].[glaspoduzetnikah_idea].[vwpayments]
